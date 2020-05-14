f13200e6457adb0ef95da64516f9587f2802de93
Suvankar <suvankarpradhan0@gmail.com>
2020-05-14 22:21:29 +0530

    add index page

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -20,7 +20,8 @@ namespace Library_Management_System
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/Custom.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
