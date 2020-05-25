dadfe130e455192c2d3e8f3c7a3e4ca84ae3cdc9
Suvankar <suvankarpradhan0@gmail.com>
2020-05-25 09:37:27 +0530

    modify book create,edit,detail page

--- Models/BookRecord.cs
+++ Models/BookRecord.cs
@@ -8,13 +8,19 @@ namespace Library_Management_System.Models
 {
     public class BookRecord
     {
+        [Display(Name ="Book Id")]
         public int book_id { get; set; }
-        [Required(ErrorMessage ="Please Enter Book Name")]
+
+        [Required(ErrorMessage ="Please Enter Book Name")][Display(Name = "Book Name")]
         public string book_name { get; set; }
+
+        [Display(Name = "Author Name")]
         public string author_name { get; set; }
-        [Required(ErrorMessage ="Please Enter Book Category")]
+
+        [Required(ErrorMessage ="Please Enter Book Category")][Display(Name = "Category")]
         public string category { get; set; }
-        [Required(ErrorMessage ="Please Enter number of Copies")]
+
+        [Required(ErrorMessage ="Please Enter number of Copies")][Display(Name = "No of Copies")]
         public int copies { get; set; }
     }
 }
